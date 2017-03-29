05a05a0aab987d2fe382e1bedd8ea92fd9530507
月球王子 <alvin198761@163.com>
2017-03-29 17:43:29 +0800

    加上crash

--- spring_mvc_endpoint/src/main/java/org/alvin/home/mvc/config/AppConfig.java
+++ spring_mvc_endpoint/src/main/java/org/alvin/home/mvc/config/AppConfig.java
@@ -1,21 +1,27 @@
 package org.alvin.home.mvc.config;
 
+import org.crsh.shell.impl.command.CRaSH;
 import org.springframework.boot.actuate.autoconfigure.EndpointAutoConfiguration;
 import org.springframework.boot.actuate.autoconfigure.HealthIndicatorAutoConfiguration;
 import org.springframework.boot.actuate.autoconfigure.MetricRepositoryAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @Import({EndpointAutoConfiguration.class, HealthIndicatorAutoConfiguration.class, MetricRepositoryAutoConfiguration.class})
-@ComponentScan(basePackages = {"org.alvin.home.mvc.component"})
+@ComponentScan(basePackages = {"org.alvin.home.mvc.component", "org.springframework.boot.starter.remote.shell"})
+@PropertySource("classpath:application.properties")
 @EnableWebMvc
 public class AppConfig {
 
-    public AppConfig(){
+    public AppConfig() {
         System.out.println("App");
-    }
+        new Thread(()->{
+            CRaSH cRaSH = new CRaSH();
+        }).start();
 
+    }
 }
--- spring_mvc_endpoint/src/main/java/org/alvin/home/mvc/config/WebAppInitializer.java
+++ spring_mvc_endpoint/src/main/java/org/alvin/home/mvc/config/WebAppInitializer.java
@@ -1,20 +1,30 @@
 package org.alvin.home.mvc.config;
 
-import javax.servlet.ServletContext;  
-import javax.servlet.ServletException;  
-import javax.servlet.ServletRegistration.Dynamic;  
-  
-import org.springframework.web.WebApplicationInitializer;  
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;  
-import org.springframework.web.servlet.DispatcherServlet;  
-  
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration.Dynamic;
+
+import org.crsh.standalone.CRaSH;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
 public class WebAppInitializer implements WebApplicationInitializer {
-	public void onStartup(ServletContext servletContext) throws ServletException {  
-        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();  
-        ctx.register(AppConfig.class);  
-        ctx.setServletContext(servletContext);    
-        Dynamic dynamic = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));  
-        dynamic.addMapping("/");  
-        dynamic.setLoadOnStartup(1);  
-   }  
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+        ctx.register(AppConfig.class);
+        ctx.setServletContext(servletContext);
+        Dynamic dynamic = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));
+        dynamic.addMapping("/");
+        dynamic.setLoadOnStartup(1);
+
+
+        new Thread(() -> {
+            try {
+                CRaSH.main(new String[]{});
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
 }
